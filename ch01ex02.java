2f235a54b8d51a2a405d3423674910cb0c868e56
Mikhail Firulin <mfirulin@gmail.com>
2016-07-28 21:33:29 +0300

    Update ch01ex02.java

--- ch01ex02.java
+++ ch01ex02.java
@@ -10,3 +10,19 @@ else
 endif
 */
 
+import java.util.Scanner;
+
+
+public class Test {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    
+    System.out.print("Enter an angle: ");
+    int a = in.nextInt();
+    // Realization with floorMod()
+    System.out.println(Math.floorMod(angle, 360));
+    // Realization with '%'
+    if (angle > 0)
+    	System.out.println(angle % 360);
+  }
+}
