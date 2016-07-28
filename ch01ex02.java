9bc06898b123d9d329afc127266f0d0995fdd541
Mikhail Firulin <mfirulin@gmail.com>
2016-07-28 21:18:36 +0300

    Create ch01ex02.java

---
+++ ch01ex02.java
@@ -0,0 +1,12 @@
+/*
+if (f.fin1 >= 0) && (f.fin1 <= 360) 
+  set f.fout to f.fin1 
+else 
+  if f.fin1 < 0 
+   set f.fin1 to (f.fin1 + 360) 
+  else 
+   set f.fin1 to (f.fin1 - 360) 
+  endif
+endif
+*/
+
