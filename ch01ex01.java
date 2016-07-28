029cdbfc35f21ecbbe4ff77f6b6ec147c4fa0530
Mikhail Firulin <mfirulin@gmail.com>
2016-07-28 20:41:52 +0300

    Create ch01ex01.java

---
+++ ch01ex01.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Test {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    
+    System.out.print("Enter an integer number: ");
+    int a = in.nextInt();
+    System.out.printf("Number: %d %<o %<X %s\n
+                       Inverse number: %f %<a\n", 
+                       a, Integer.toString(a, 2), 1.0/a);
+  }
+}
