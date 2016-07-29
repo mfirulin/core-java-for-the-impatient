b4d0232b30950bf09fcbd706c7fbea23af035f9b
Mikhail Firulin <mfirulin@gmail.com>
2016-07-29 10:49:08 +0300

    Update ch01ex02.java

--- ch01ex02.java
+++ ch01ex02.java
@@ -1,28 +1,19 @@
-/*
-if (f.fin1 >= 0) && (f.fin1 <= 360) 
-  set f.fout to f.fin1 
-else 
-  if f.fin1 < 0 
-   set f.fin1 to (f.fin1 + 360) 
-  else 
-   set f.fin1 to (f.fin1 - 360) 
-  endif
-endif
-*/
-
 import java.util.Scanner;
-
 
 public class Test {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     
     System.out.print("Enter an angle: ");
-    int a = in.nextInt();
+    int angle = in.nextInt();
+    
     // Realization with floorMod()
     System.out.println(Math.floorMod(angle, 360));
+    
     // Realization with '%'
-    if (angle > 0)
-    	System.out.println(angle % 360);
+    angle %= 360;
+    if (angle < 0)
+      angle += 360;
+    System.out.println(angle);
   }
 }
