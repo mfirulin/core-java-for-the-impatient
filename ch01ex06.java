5cf48a83312f7ddd4581d702cade5bb870c61ca9
Mikhail Firulin <mfirulin@gmail.com>
2016-07-29 19:55:23 +0300

    Create ch01ex06.java

---
+++ ch01ex06.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+
+public class Test {
+  public static void main(String[] args) {
+  	BigInteger n = BigInteger.valueOf(1);
+    
+    for (int i = 2; i <= 1000; i++)
+      n = n.multiply(BigInteger.valueOf(i));
+ 	System.out.println(n);
+  }
+}
