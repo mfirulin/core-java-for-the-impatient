85852bc49c191ce880f2d4e453f9b00ec097548e
Mikhail Firulin <mfirulin@gmail.com>
2016-07-31 20:01:58 +0400

    Update Ch01Ex07.java

--- Ch01Ex07.java
+++ Ch01Ex07.java
@@ -1,28 +1,17 @@
 import java.util.Scanner;
 
 public class Ch01Ex07 {
-  public static void main(String args[]) {
-    /*
-  	Scanner in = new Scanner(Sistem.in);
-    System.out.print("Enter the first number (from 0 to 65535): ");
-    short a = in.nextShort();
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter the first number (from 0 to 65535): ");       
+    short a = (short)in.nextInt();
     System.out.print("Enter the second number (from 0 to 65535): ");
-    short b = in.nextShort();
-    */
-    
-    short a = 65530;
-    short b = 2;
-    
-    short sum = (short)(a + b);
-    short difference = (short)(a - b);
-    short product = (short)(a * b);
-    short quotient = (short)(a / b);
-    short remainder = (short)(a % b);
-    
-    System.out.println("a + b = " + sum);
-    System.out.println("a - b = " + difference);
-    System.out.println("a * b = " + product);
-    System.out.println("a / b = " + quotient);
-    System.out.println("a % b = " + remainder);
+    short b = (short)in.nextInt();
+        
+    System.out.println("a + b = " + Short.toUnsignedInt((short)(a + b)));
+    System.out.println("a - b = " + Short.toUnsignedInt((short)(a - b)));
+    System.out.println("a * b = " + Short.toUnsignedInt(a) * Short.toUnsignedInt(b));
+    System.out.println("a / b = " + Short.toUnsignedInt(a) / Short.toUnsignedInt(b));
+    System.out.println("a % b = " + Short.toUnsignedInt(a) % Short.toUnsignedInt(b));
   }
 }
