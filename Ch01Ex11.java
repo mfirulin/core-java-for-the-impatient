e19a815e8287656d1c8b459bb94f4b928fafa724
Mikhail Firulin <mfirulin@gmail.com>
2016-08-02 20:39:07 +0400

    Update Ch01Ex11.java

--- Ch01Ex11.java
+++ Ch01Ex11.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 
 public class Ch01Ex11 {
   public static void main(String[] args) {
+    System.out.print("Enter a line: ");
   	Scanner in = new Scanner(System.in);
     String str = in.nextLine();
     int[] codePoints = str.codePoints().toArray();
     
     for (int i : codePoints) {
-    	System.out.print(i + " ");
+    	System.out.print(i + ": ");
+    	System.out.println((char)i);
     }
   }
 }
