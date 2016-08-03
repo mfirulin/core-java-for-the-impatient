43f7ba255b0b7a4bcf7a9330483be9be797e34e5
Mikhail Firulin <mfirulin@gmail.com>
2016-08-03 22:11:17 +0400

    Update Ch01Ex14.java

--- Ch01Ex14.java
+++ Ch01Ex14.java
@@ -2,35 +2,72 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Ch01Ex14 {
+
+  private static final int SIZE = 5;
+  
   public static void main(String[] args) {
-    String line;
-    int[][] square = null;
+    int rows = 0, columns = 0, sum = 0;
+    int[][] square = new int[SIZE][SIZE];
     Scanner in = new Scanner(System.in);
     
-    do {
-      int i = 0;
-      boolean squareIsCreated = false;
-      line = in.nextLine();
-      line = line.trim();
+    for (; rows < SIZE; rows++) {
+      String line = in.nextLine().trim();
       String[] words = line.split("\\s+");
       
-      if (!squareIsCreated) {
-        square = new int[words.length][words.length];
-        squareIsCreated = true;
-      }
-      
-      if (!line.isEmpty())
-        for (int j = 0; j < words.length; j++) 
-          square[i][j] = Integer.parseInt(words[j]);
-
-      i++;
-    } while (!line.isEmpty());
-    
-    for (int i = 0; i < square.length; i++) {
-      for (int j = 0; j < square[i].length; j++)
-        System.out.print(square[i][j] + " ");
-      System.out.println();
+      if (line.isEmpty())
+        break;
+        
+      for (columns = 0; columns < words.length && columns < SIZE; columns++) 
+        square[rows][columns] = Integer.parseInt(words[columns]);
     }
-    System.out.println(Arrays.deepToString(square));
+    
+   //System.out.println(Arrays.deepToString(square));
+
+    // Find control sum
+    for (int a : square[0])
+      sum += a;
+    
+    // Check rows
+    for (int i = 1; i < rows; i++) {
+      int s = 0;
+      for (int j = 0; j < columns; j++)
+        s += square[i][j];
+
+      if (s != sum) {
+        System.out.println("Not magic");
+        return;
+      }
+    }
+    
+    // Check columns
+    for (int j = 0; j < columns; j++) {
+      int s = 0;
+      for (int i = 0; i < rows; i++)
+        s += square[i][j];
+ 
+      if (s != sum) {
+        System.out.println("Not magic");
+        return;
+      }
+    }
+    
+    // Check dioganals
+    int s = 0;
+    for (int i = 0, j = 0; i < rows; i++, j++)
+        s += square[i][j];
+    
+    if (s != sum) {
+        System.out.println("Not magic");
+        return;
+    }
+    
+    s = 0;
+    for (int i = rows - 1, j = 0; i >= 0; i--, j++)
+        s += square[i][j];
+     
+    if (s != sum) {
+        System.out.println("Not magic");
+        return;
+    }
   }
 }
