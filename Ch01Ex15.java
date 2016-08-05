a361885e822d1707f3bbf215fa292a249f4a07f9
Mikhail Firulin <mfirulin@gmail.com>
2016-08-05 15:41:01 +0300

    Update Ch01Ex15.java

--- Ch01Ex15.java
+++ Ch01Ex15.java
@@ -14,7 +14,7 @@ public class Ch01Ex15 {
       for (int j = 1; j < i; j++) {
       	row.add(triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j));
       }
-      if(i != 0) {
+      if(i > 0) {
       	row.add(1);
       }
       triangle.add(row);
