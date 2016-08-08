660040141ddd28c65d288918b012bfa968ca242e
Mikhail Firulin <mfirulin@gmail.com>
2016-08-08 19:31:58 +0300

    Create Ch02Ex05.java

---
+++ Ch02Ex05.java
@@ -0,0 +1,36 @@
+public class Point {
+  private double x, y;
+  
+  public Point(double x, double y) {
+  	this.x = x;
+    this.y = y;
+  }
+  
+  public Point() {
+  	this(0, 0);
+  }
+  
+  public double getX() {
+  	return x;
+  }
+  
+  public double getY() {
+  	return y;
+  }
+  
+  public Point translate(double x, double y) {
+  	return new Point(this.x + x, this.y + y);
+  }
+  
+  public Point scale(double c) {
+  	return new Point(x*c, y*c);
+  }
+  
+  
+  public static void main(String[] args) {
+  	Point p = new Point(3, 4).translate(1, 3).scale(0.5);
+    System.out.println(p.getX() + " " + p.getY());
+    p = new Point().translate(-1, 3).scale(2.5);
+    System.out.println(p.getX() + " " + p.getY());
+  }
+}
