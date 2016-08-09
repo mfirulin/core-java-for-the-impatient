7bbfaaa06496fc75b3d3da49e98cc1eb8bdcfd53
Mikhail Firulin <mfirulin@gmail.com>
2016-08-09 15:58:50 +0300

    Update Ch02Ex09.java

--- Ch02Ex09.java
+++ Ch02Ex09.java
@@ -3,20 +3,22 @@
 * @author Mikhail Firulin
 * @version 1.0
 */
-
 public class Car {
-  private final double fuelСonsumption; /* Litres per kilometre */
+  private final double consumption; /* Fuel consumption (litres per kilometre) */
+  private final double tank; /* Max volume of the tank (litres) */
   private double fuel; /* Amount of fuel in the tank (litres) */
   private double coordinate; /* Distance from the origin of coordinates (kilometres) */
   
   /**
-  * Create a car with predefined fuel consumption
-  * @param fuelConsumption Fuel consumption (litres per kilometre)
+  * Create a car with predefined volume of the tank and fuel consumption
+  * @param tank Max volume of the tank (litres)
+  * @param consumption Fuel consumption (litres per kilometre)
   */
-  public Car(double fuelConsumption) {
-  	this.fuelConsumption = fuelConsumption;
+  public Car(double tank, double consumption) {
+  	this.tank = tank;
+    this.consumption = consumption;
   }
-  
+
   /**
   * Get amount of fuel in the tank (litres).
   * @return Amount of fuel in the tank (litres)
@@ -38,23 +40,41 @@ public class Car {
   * @param fuel Amount of fuel to add (litres)
   */
   public void refuel(double fuel) {
-  	this.fuel += fuel;
+    if (this.fuel + fuel > tank)
+      this.fuel = tank;
+    else
+  	  this.fuel += fuel;
   }
   
   /**
-  * Move a car 
+  * Move a car to distance or until fuel ends.
   * @param distance Distance to move (kilometres)
   */
   public void move(double distance) {
-  	coordinate += distance;
-    fuel -= distance * fuelConsumption;
+    double expectedConsumption = distance * consumption;
+    
+    if(expectedConsumption > fuel) {
+      coordinate += fuel / consumption;
+      fuel = 0;
+    }
+    else {
+  	  coordinate += distance;
+      fuel -= expectedConsumption;
+    }
+      
   }
   
   public static void main(String[] args) {
-  	Car car = new Car(0.08);
+  	Car car = new Car(50, 0.1);
     car.refuel(50);
+    System.out.println(car.getCoordinate());
     System.out.println(car.getFuel());
     car.move(100);
     System.out.println(car.getCoordinate());
+    System.out.println(car.getFuel());
+    car.refuel(100);
+    car.move(1600);
+    System.out.println(car.getCoordinate());
+    System.out.println(car.getFuel());
   }
 }
