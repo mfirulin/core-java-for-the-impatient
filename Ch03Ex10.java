6faf3b8d42c70db865bafb160b67e62183da9145
Mikhail Firulin <mfirulin@gmail.com>
2016-08-18 16:13:41 +0300

    Create Ch03Ex10.java

---
+++ Ch03Ex10.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.util.Arrays;
+
+public class Ch03Ex10 {
+  
+  private static void printSubDirs(String name) {
+  	File[] dirs = new File(name).listFiles((f) -> f.isDirectory());
+    
+    if (dirs == null) return;
+    
+    for (File dir : dirs) {
+      String dirName = dir.getName();
+      String pathName = dir.getPath();
+      System.out.println(dirName);
+      printSubDirs(pathName);
+    }
+  }
+
+  public static void main(String[] args) {
+    if (args.length > 0)
+      printSubDirs(args[0]);
+    else
+  	  printSubDirs(".");
+  }
+}
