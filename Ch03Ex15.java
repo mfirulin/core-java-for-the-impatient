c9ebfbf852ac97c2177e5ac5d9ba8f47544897c8
Mikhail Firulin <mfirulin@gmail.com>
2016-08-20 21:33:37 +0400

    Create Ch03Ex15.java

---
+++ Ch03Ex15.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+interface IntSequence {
+    default boolean hasNext() { return true; }
+    int next();
+}
+
+public class Ch03Ex15 {
+    private static Random generator = new Random();
+    
+    public static class RandomSequence implements IntSequence {
+        int low, high;
+        public RandomSequence(int low, int high) {
+            this.low = low;
+            this.high = high;
+        }
+        public int next() { return low + generator.nextInt(high - low + 1); }
+    }
+    
+    public static IntSequence randomInts(int low, int high) {
+        return new RandomSequence(low, high);
+    }
+    
+    public static void main(String[] args) {
+        IntSequence is = randomInts(1, 6);
+        for (int i = 0; i < 10; i++)
+            System.out.println(is.next());
+    }
+}
