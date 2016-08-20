b59e479a7c1369deedb4798dd7c868c32444551c
Mikhail Firulin <mfirulin@gmail.com>
2016-08-20 19:17:30 +0300

    Update Ch03Ex14.java

--- Ch03Ex14.java
+++ Ch03Ex14.java
@@ -27,13 +27,14 @@ class Employee {
 
 public class Ch03Ex14 {
   public static void main(String[] args) {
-  	Employee[] employees = new Employee[6];
-    employees[0] = new Employee("Fred", 1250);
-    employees[1] = new Employee("Anna", 1150);
-    employees[2] = new Employee("John", 1300);
-    employees[3] = new Employee("Allan", 1050);
-    employees[4] = new Employee("Mark", 1200);
-    employees[5] = new Employee("Joe", 1400);
+    Employee[] employees = new Employee[] {
+      new Employee("Fred", 1250),
+      new Employee("Anna", 1150),
+      new Employee("John", 1300),
+      new Employee("Allan", 1050),
+      new Employee("Mark", 1200),
+      new Employee("Joe", 1400)
+    };
     
     Arrays.sort(employees, Comparator.comparing(Employee::getSalary).thenComparing(Employee::getName));
     // Arrays.sort(employees, Comparator.comparing((Employee e) -> e.getSalary()).thenComparing((Employee e) -> e.getName()));
