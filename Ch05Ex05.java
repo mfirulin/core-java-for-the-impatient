99d9e80478c8edf74b747702866e247c9719c2e8
Mikhail Firulin <mfirulin@gmail.com>
2016-09-24 21:24:29 +0400

    Create Ch05Ex05.java

---
+++ Ch05Ex05.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+public class Ch05Ex05 {
+    public static void readWriteValues(String input, String output) {
+        Scanner in = null;
+        PrintWriter out = null;
+        
+        try {
+            in = new Scanner(Paths.get(input));
+        }
+        catch (IOException ex) {
+            System.err.println(ex);
+            return;
+        }
+        
+        try {
+            out = new PrintWriter(output);
+        }
+        catch (FileNotFoundException ex) {
+            System.err.println(ex);
+            return;
+        }
+        
+        try {
+            while(in.hasNext()) {
+                out.println(in.next());
+            }   
+        }
+        catch (IllegalStateException ex1) {}
+        
+        if (in != null) in.close();
+        if (out != null) out.close();
+    }
+
+    public static void main(String[] args) {
+        readWriteValues("input.txt", "output.txt");
+    }
+}
