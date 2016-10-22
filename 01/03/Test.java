6382202fb9b6d8bc97f9719f4603e918b8e6d445
Mikhail Firulin <mfirulin@gmail.com>
2016-10-22 17:43:04 +0200

    Create Test.java

---
+++ 01/03/Test.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        int result;
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Enter the first number: ");
+        int a = in.nextInt();
+        System.out.print("Enter the second number: ");
+        int b = in.nextInt();
+        System.out.print("Enter the third number: ");
+        int c = in.nextInt();
+        
+        // Realization with 'if'
+        if (b < a && a > c) result = a;
+        else if (a < b && b > c) result = b;
+        else result = c;
+        System.out.println(result);
+        
+        // Realizaion with Math.max()
+        result = Math.max(a, b);
+        result = Math.max(result, c);
+        System.out.println(result);
+    }
+}
