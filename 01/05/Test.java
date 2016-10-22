1d115e8530558785b122c1f563dbdc9a483e049f
Mikhail Firulin <mfirulin@gmail.com>
2016-10-22 17:53:30 +0200

    Create Test.java

---
+++ 01/05/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        double d = 1_000_000_000_000_000_000L;
+        int i = (int)d;
+        System.out.println(d);
+        System.out.println(i);
+    }
+}
