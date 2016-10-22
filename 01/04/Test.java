4c6a506859a2141a64a3be6502c25a0e8f3e04e7
Mikhail Firulin <mfirulin@gmail.com>
2016-10-22 17:49:14 +0200

    Create Test.java

---
+++ 01/04/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Math.nextUp(0.0));
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Math.nextDown(Double.POSITIVE_INFINITY));
+        System.out.println(Math.nextUp(Double.NEGATIVE_INFINITY));
+    }
+}
