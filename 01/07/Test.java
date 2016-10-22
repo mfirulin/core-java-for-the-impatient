62a84688e6058d06a0a422f8c8f07fcb9bcc3561
Mikhail Firulin <mfirulin@gmail.com>
2016-10-22 18:06:05 +0200

    Create Test.java

---
+++ 01/07/Test.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the first number (from 0 to 65535): ");       
+        short a = (short)in.nextInt();
+        System.out.print("Enter the second number (from 0 to 65535): ");
+        short b = (short)in.nextInt();
+        
+        System.out.println("a + b = " + Short.toUnsignedInt((short)(a + b)));
+        System.out.println("a - b = " + Short.toUnsignedInt((short)(a - b)));
+        System.out.println("a * b = " + Short.toUnsignedInt(a) * Short.toUnsignedInt(b));
+        System.out.println("a / b = " + Short.toUnsignedInt(a) / Short.toUnsignedInt(b));
+        System.out.println("a % b = " + Short.toUnsignedInt(a) % Short.toUnsignedInt(b));
+    }
+}
