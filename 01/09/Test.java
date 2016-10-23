08dc849595b96a9746021b574c0aefbee7d92a36
Mikhail Firulin <mfirulin@gmail.com>
2016-10-23 19:36:22 +0200

    Create Test.java

---
+++ 01/09/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+	    String str1 = "Hello, World!";
+        String str2 = str1.substring(7, 12);
+        String str3 = "World";
+        String str4 = new String(str2);
+    
+        System.out.println(String.join(", ", str2, str3, str4));
+        System.out.println("World".equals(str2) && "World".equals(str3) && "World".equals(str4)); // True
+        System.out.println(str2 == str3 || str2 == str4 || str3 == str4); // False
+    }
+}
