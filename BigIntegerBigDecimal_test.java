e6b1cc85ac6db75b10992a0f181917003a393c2b
Mikhail Firulin <mfirulin@gmail.com>
2016-10-26 21:19:06 +0200

    Update BigIntegerBigDecimal_test.java

--- BigIntegerBigDecimal_test.java
+++ BigIntegerBigDecimal_test.java
@@ -2,10 +2,10 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class Test {
-  public static void main(String[] args) {
-    System.out.println(2.0 - 1.1);
-    System.out.println(BigDecimal.valueOf(2.0).subtract(BigDecimal.valueOf(1.1)));
-    System.out.println(Long.MAX_VALUE + 1);
-    System.out.println(new BigInteger("9223372036854775808"));
-  }
+    public static void main(String[] args) {
+        System.out.println(2.0 - 1.1);
+        System.out.println(BigDecimal.valueOf(2.0).subtract(BigDecimal.valueOf(1.1)));
+        System.out.println(Long.MAX_VALUE + 1);
+        System.out.println(new BigInteger("9223372036854775808"));
+    }
 }
