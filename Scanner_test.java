7c7bc438e0994f443628264ecc98695cc25e565f
Mikhail Firulin <mfirulin@gmail.com>
2016-10-26 21:24:51 +0200

    Update Scanner_test.java

--- Scanner_test.java
+++ Scanner_test.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
 public class Test {
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-  
-    while (true) {
-      System.out.print("Press q/Q to exit: ");
-      if (in.hasNext()) {
-        String input = in.next();
-      	if ("q".equalsIgnoreCase(input))
-        	break;
-      }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Press q/Q to exit: ");
+            if (in.hasNext()) {
+                String input = in.next();
+                if ("q".equalsIgnoreCase(input)) break;
+            }
+        }
     }
-  }
 }
