4c1c4aa0c367aefbabf5e939bd4631437356725f
Mikhail Firulin <mfirulin@gmail.com>
2017-11-29 19:30:05 +0300

    Update and rename Ch03Ex09.java to chapter03/exercise09/Test.java

---
+++ chapter03/exercise09/Test.java
@@ -0,0 +1,35 @@
+public class Test implements Runnable {
+    private String name;
+  
+    public Test(String name) {
+  	    this.name = name;
+    }
+  
+    public void run() {
+  	    System.out.println("I am " + name);
+    }
+
+    public static void runTogether(Runnable... tasks) {
+        for (Runnable task : tasks) {
+            Thread thread = new Thread(task);
+            thread.start();
+        }
+    }
+  
+    public static void runInOrder(Runnable... tasks) {
+        for (Runnable task: tasks)
+            task.run();
+    }
+  
+    public static void main(String[] args) {
+  	    Runnable[] tasks = new Runnable[3];
+        tasks[0] = new Test("Scott");
+        tasks[1] = new Test("Peter");
+        tasks[2] = new Test("Anna");
+
+        System.out.println("runInOrder() works:");
+        runInOrder(tasks);
+        System.out.println("runTogether() works:");
+        runTogether(tasks);
+    }
+}
