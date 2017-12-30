b90135fe0f4a6b7f7e8d086961b96ab67d0c078e
Mikhail Firulin <mfirulin@gmail.com>
2017-12-30 22:22:22 +0300

    Update LabeledPoint.java

--- chapter04/exercise02/LabeledPoint.java
+++ chapter04/exercise02/LabeledPoint.java
@@ -4,7 +4,7 @@ class Point {
     private double x, y;
   
     public Point(double x, double y) {
-  	    this.x = x;
+        this.x = x;
         this.y = y;
     }
   
@@ -12,7 +12,7 @@ class Point {
     public double getY() { return y; }
   
     public String toString() {
-  	    return getClass().getName() + "[x=" + x + ",y=" + y + "]";
+        return getClass().getName() + "[x=" + x + ",y=" + y + "]";
     }
   
     public boolean equals(Object other) {
@@ -24,7 +24,7 @@ class Point {
     }
   
     public int hashCode() {
-  	    return Objects.hash(x, y);
+        return Objects.hash(x, y);
     }
 }
 
@@ -33,28 +33,28 @@ public class LabeledPoint extends Point {
     private String label;
   
     public LabeledPoint(String label, double x, double y) {
-  	    super(x, y);
+        super(x, y);
         this.label = label;
     }
   
     public String getLabel() { return label; }
   
     public String toString() {
-  	    return super.toString() + "[label=" + label + "]"; 
+        return super.toString() + "[label=" + label + "]"; 
     }
   
     public boolean equals(Object other) {
-  	    if (!super.equals(other)) return false;
+        if (!super.equals(other)) return false;
         LabeledPoint otherLabeledPoint = (LabeledPoint)other;
         return Objects.equals(label, otherLabeledPoint.label);
     }
   
     public int hashCode() {
-  	    return Objects.hash(super.hashCode(), label);
+        return Objects.hash(super.hashCode(), label);
     }
   
     public static void main(String[] args) {
-  	    LabeledPoint lpoint1 = new LabeledPoint("LabeledPoint", 1.0, 1.0);
+        LabeledPoint lpoint1 = new LabeledPoint("LabeledPoint", 1.0, 1.0);
         System.out.println(lpoint1);
         LabeledPoint lpoint2 = new LabeledPoint("LabeledPoint", 1.0, 1.0);
         System.out.println(lpoint1.equals(lpoint2));
