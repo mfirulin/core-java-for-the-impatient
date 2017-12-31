3ffb7c3cec9b69123ab584eb327173b19de72411
Mikhail Firulin <mfirulin@gmail.com>
2017-12-31 11:07:14 +0300

    Update and rename Ch04Ex09.java to chapter04/exercise09/Test.java

---
+++ chapter04/exercise09/Test.java
@@ -0,0 +1,50 @@
+import java.lang.reflect.Field;
+
+class Item {
+    private int id;
+    private String description;
+    private double price;
+  
+    public Item(int id, String description, double price) {
+        this.id = id;
+        this.description = description;
+        this.price = price;
+    }
+  
+    public String toString() {
+        return getClass().getName() + "[id=" + id + 
+            ",description=" + description + ",price=" + price + "]";
+    }
+}
+  
+
+public class Test {
+    public static String toString(Object obj) {
+        Class<?> cl = obj.getClass();
+  	    String str = cl.getName();
+        str += "[";
+    
+        for (Field f : cl.getDeclaredFields()) {
+            Object value = 0;
+            f.setAccessible(true);
+            try {
+                value = f.get(obj);
+            }
+            catch (IllegalAccessException ex) {
+                System.out.println("No access");
+            }
+            str += (f.getName() + "=" + value + ",");
+        }
+    
+        str += "]";
+        return str;
+    }
+  
+    public static void main(String[] args) {
+        Item item = new Item(1, "solt", 0.2);
+    
+        System.out.println(item);
+        System.out.println(toString(item));
+    }
+}
+  
