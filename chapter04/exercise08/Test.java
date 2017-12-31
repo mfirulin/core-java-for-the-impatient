9a6067cf766f927a34ba537fde77809882ae2720
Mikhail Firulin <mfirulin@gmail.com>
2017-12-31 10:45:16 +0300

    Update and rename Ch04Ex08.java to chapter04/exercise08/Test.java

---
+++ chapter04/exercise08/Test.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+
+public class Test {
+  
+    private class InternalClass { }
+  
+    private static class InternalStaticClass { }
+  
+    public static void main(String[] args) {
+        Class<?> cl = String[].class;
+  	    System.out.println(cl.getName());
+        System.out.println(cl.getSimpleName());
+        System.out.println(cl.getCanonicalName());
+        System.out.println(cl.getTypeName());
+        System.out.println(cl.toString());
+        System.out.println(cl.toGenericString());
+        System.out.println();
+    
+        cl = Integer[].class;
+  	    System.out.println(cl.getName());
+        System.out.println(cl.getSimpleName());
+        System.out.println(cl.getCanonicalName());
+        System.out.println(cl.getTypeName());
+        System.out.println(cl.toString());
+        System.out.println(cl.toGenericString());
+        System.out.println();
+    
+        cl = int.class;
+  	    System.out.println(cl.getName());
+        System.out.println(cl.getSimpleName());
+        System.out.println(cl.getCanonicalName());
+        System.out.println(cl.getTypeName());
+        System.out.println(cl.toString());
+        System.out.println(cl.toGenericString());
+        System.out.println();
+    
+        cl = Test.class;
+  	    System.out.println(cl.getName());
+        System.out.println(cl.getSimpleName());
+        System.out.println(cl.getCanonicalName());
+        System.out.println(cl.getTypeName());
+        System.out.println(cl.toString());
+        System.out.println(cl.toGenericString());
+        System.out.println();
+
+        cl = InternalClass.class;
+  	    System.out.println(cl.getName());
+        System.out.println(cl.getSimpleName());
+        System.out.println(cl.getCanonicalName());
+        System.out.println(cl.getTypeName());
+        System.out.println(cl.toString());
+        System.out.println(cl.toGenericString());
+        System.out.println();
+    
+        cl = InternalStaticClass.class;
+  	    System.out.println(cl.getName());
+        System.out.println(cl.getSimpleName());
+        System.out.println(cl.getCanonicalName());
+        System.out.println(cl.getTypeName());
+        System.out.println(cl.toString());
+        System.out.println(cl.toGenericString());
+        System.out.println();
+    
+        cl = ArrayList.class;
+        System.out.println(cl.getName());
+        System.out.println(cl.getSimpleName());
+        System.out.println(cl.getCanonicalName());
+        System.out.println(cl.getTypeName());
+        System.out.println(cl.toString());
+        System.out.println(cl.toGenericString());
+        System.out.println();
+    }
+}
