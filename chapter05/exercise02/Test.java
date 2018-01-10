c1d7ff2dd181f440317acbe0b09fb045953f125d
Mikhail Firulin <mfirulin@mera.ru>
2018-01-10 19:35:44 +0300

    Chapter 5. Exercise 2.

---
+++ chapter05/exercise02/Test.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+public class Test {
+    public static ArrayList<Double> readValues(String filename)
+        throws IOException
+    {
+        ArrayList<Double> numbers = new ArrayList<>();
+        
+        try (Scanner in = new Scanner(Paths.get(filename))) {
+            while(in.hasNextDouble())
+                numbers.add(in.nextDouble());
+        }
+        return numbers;
+    }
+
+    public static double sumOfValues(String filename)
+        throws IOException
+    {
+		ArrayList<Double> numbers;
+		double sum = 0;
+		
+		try {
+			numbers = readValues(filename);
+        }
+		catch (IOException ex) {
+			throw ex;
+        }
+
+		for (double n : numbers) 
+			sum += n;
+		
+        return sum;
+    }
+    
+    public static void main(String[] args) {
+        try {
+            double sum = sumOfValues("Test.txt");
+            System.out.println(sum);
+        }
+        catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
+}
