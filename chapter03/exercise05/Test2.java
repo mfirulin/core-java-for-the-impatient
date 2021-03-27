b7ec11c4f7a66365bf0b83de42662c202c97b5b8
Mikhail Firulin <mfirulin@gmail.com>
2021-03-27 12:54:57 +0300

    Update Test2.java

--- chapter03/exercise05/Test2.java
+++ chapter03/exercise05/Test2.java
@@ -7,7 +7,7 @@ interface IntSequence {
 
 public class Test2 {
     public static void main(String[] args) {
-   	    IntSequence constant = IntSequence.constant(3);
+        IntSequence constant = IntSequence.constant(3);
     
         for(int i = 0; i < 10; i++)
             System.out.println(constant.next());
